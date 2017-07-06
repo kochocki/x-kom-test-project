028c1a1eee2488cb40191b324884466bc24eb4b5
Krzysztof Ochocki <krzysztof.ochocki1@gmail.com>
2017-07-06 23:13:51 +0200

    added equals method without quantity comparing

--- src/main/java/pageModels/Product.java
+++ src/main/java/pageModels/Product.java
@@ -35,14 +35,11 @@ public class Product {
 		this.quantity = quantity;
 	}
 
-	@Override
-	public boolean equals(Object other) {
+	public boolean equalsWithoutQuantity(Object other) {
 		if (!(other instanceof Product)) {
 			return false;
 		}
-
 		Product that = (Product) other;
-
 		return this.name.equals(that.name) && this.price.equals(that.price);
 	}
 
@@ -50,4 +47,39 @@ public class Product {
 	public String toString() {
 		return "Product [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
+
 }
