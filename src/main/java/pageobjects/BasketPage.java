79eb7d077379954e57898bc56e28c68a2625315c
Krzysztof Ochocki <krzysztof.ochocki1@gmail.com>
2017-07-06 21:12:39 +0200

    added incrementing and decrementing by button, loader handling

--- src/main/java/pageobjects/BasketPage.java
+++ src/main/java/pageobjects/BasketPage.java
@@ -22,24 +22,32 @@ public class BasketPage extends Header {
 	private WebElement summaryPrice;
 	@FindBys({ @FindBy(css = ".basket-item") })
 	private List<WebElement> productsWE;
+	@FindBys({ @FindBy(css = ".basket-item>.totalprice-wrapper>.remove-product") })
+	private List<WebElement> deleteProductButtons;
+	@FindBys({ @FindBy(css = ".basket-item>.count-wrapper>span>.js-quantity-increment") })
+	private List<WebElement> incrementQuantityButtons;
+	@FindBys({ @FindBy(css = ".basket-item>.count-wrapper>span>.js-quantity-decrement") })
+	private List<WebElement> decrementQuantityButtons;
+	@FindBy(className = "spinner")
+	private WebElement loader;
 
 	public BasketPage() {
 		super();
-		isLoaded(basket);
+		waitUntilVisible(basket);
 	}
 
 	public String getEmptyBasketText() {
-		isLoaded(emptyBasket);
+		waitUntilVisible(emptyBasket);
 		return emptyBasket.getText();
 	}
 
 	public Double getFirstProductOverallPrice() {
-		isLoaded(firstProductOverallPrice);
+		waitUntilVisible(firstProductOverallPrice);
 		return PriceUtil.getOnlyPriceValue(firstProductOverallPrice.getText());
 	}
 
 	public Double getSummaryPrice() {
-		isLoaded(summaryPrice);
+		waitUntilVisible(summaryPrice);
 		return PriceUtil.getOnlyPriceValue(summaryPrice.getText());
 	}
 
@@ -53,6 +61,28 @@ public class BasketPage extends Header {
 			products.add(new Product(name, price, quantity));
 		}
 		return products;
+	}
+
+	public BasketPage removeProduct(int index) {
+		deleteProductButtons.get(index).click();
+		return this;
+	}
+
+	public BasketPage incrementProductQuantity(int productIndex) {
+		incrementQuantityButtons.get(productIndex).click();
+		waitForLoader();
+		return this;
+	}
+
+	public BasketPage decrementProductQuantity(int productIndex) {
+		decrementQuantityButtons.get(productIndex).click();
+		waitForLoader();
+		return this;
+	}
+
+	public void waitForLoader() {
+		waitUntilVisible(loader);
+		waitUntilInvisible(loader);
 	}
 
 }
