58eddfd6c4fa90031ab3ffe5e0fda5ab346dc520
Krzysztof Ochocki <krzysztof.ochocki1@gmail.com>
2017-07-06 18:33:50 +0200

    added getALlProducts from basket method

--- src/main/java/pageobjects/BasketPage.java
+++ src/main/java/pageobjects/BasketPage.java
@@ -3,10 +3,12 @@ package pageobjects;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 
+import pageModels.Product;
 import pageUtils.PriceUtil;
 
 public class BasketPage extends Header {
@@ -18,8 +20,8 @@ public class BasketPage extends Header {
 	private WebElement firstProductOverallPrice;
 	@FindBy(className = "price-value-label")
 	private WebElement summaryPrice;
-	@FindBys({ @FindBy(css = ".basket-item-group>.basket-item>.description-wrapper>.name") })
-	private List<WebElement> productNames;
+	@FindBys({ @FindBy(css = ".basket-item") })
+	private List<WebElement> productsWE;
 
 	public BasketPage() {
 		super();
@@ -41,12 +43,16 @@ public class BasketPage extends Header {
 		return PriceUtil.getOnlyPriceValue(summaryPrice.getText());
 	}
 
-	public List<String> getAllProductNames() {
-		List<String> names = new ArrayList<String>();
-		for (WebElement we : productNames) {
-			names.add(we.getText());
+	public List<Product> getAllProducts() {
+		List<Product> products = new ArrayList<Product>();
+		for (WebElement productWE : productsWE) {
+			String name = productWE.findElement(By.className("name")).getText();
+			Double price = PriceUtil.getOnlyPriceValue(productWE.findElement(By.className("price")).getText());
+			int quantity = Integer
+					.parseInt(productWE.findElement(By.className("js-quantity-input")).getAttribute("value"));
+			products.add(new Product(name, price, quantity));
 		}
-		return names;
+		return products;
 	}
 
 }
